697e91cffe7ceb20b2adfbe6a07f420f5fd99b7e
Aleksey Nikolaev <nikolaev@jivosite.com>
2020-08-13 08:43:14 +0300

    tests for saga

--- src/Saga/__tests__/signin.test.ts
+++ src/Saga/__tests__/signin.test.ts
@@ -1,24 +1,116 @@
 import {call, put, takeEvery} from 'redux-saga/effects';
-import {signin} from '../signin';
-import {SigninTypes} from '../../../src/Actions/signin';
+import {signin, API_RQUEST_TS} from '../signin';
+import {SigninTypes, SigninError, SigninSuccess} from '../../../src/Actions/signin';
 import signValidate from '../../Validate/signin';
+import signinRequest from 'src/API/signin';
+import sleep from '../../Utils/sleep';
 
 describe('Saga', () => {
   describe('Signin', () => {
     test('Incorrect login', () => {
+      const data = {
+        login: '',
+        password: ''
+      };
       const gen = signin({
         type: SigninTypes.SIGNIN_REQUEST,
-        payload: {
-          login: '',
-          password: ''
-        }
+        payload: data
       });
-      expect(gen.next().value).toMatchObject(
-        call(signValidate, {
-          login: '',
-          password: ''
-        })
-      );
+      expect(gen.next().value).toMatchObject(call(signValidate, data));
+      const errors = signValidate(data);
+      expect(gen.next(errors).value).toMatchObject(put(SigninError(errors)));
+      expect(gen.next().done).toBeTruthy();
+    });
+
+    test('Incorrect password', () => {
+      const data = {
+        login: 'login',
+        password: ''
+      };
+      const gen = signin({
+        type: SigninTypes.SIGNIN_REQUEST,
+        payload: data
+      });
+      expect(gen.next().value).toMatchObject(call(signValidate, data));
+      const errors = signValidate(data);
+      expect(gen.next(errors).value).toMatchObject(put(SigninError(errors)));
+      expect(gen.next().done).toBeTruthy();
+    });
+
+    test('Incorrect password 2', () => {
+      const data = {
+        login: 'login',
+        password: 'pass'
+      };
+      const gen = signin({
+        type: SigninTypes.SIGNIN_REQUEST,
+        payload: data
+      });
+      expect(gen.next().value).toMatchObject(call(signValidate, data));
+      const errors = signValidate(data);
+      expect(gen.next(errors).value).toMatchObject(put(SigninError(errors)));
+      expect(gen.next().done).toBeTruthy();
+    });
+
+    test('Wrong password', () => {
+      const data = {
+        login: 'login',
+        password: 'password'
+      };
+      const gen = signin({
+        type: SigninTypes.SIGNIN_REQUEST,
+        payload: data
+      });
+      expect(gen.next().value).toMatchObject(call(signValidate, data));
+      const errors = signValidate(data);
+      expect(gen.next(errors).value).toMatchObject(call(signinRequest, data));
+      const res = {
+        errors: ['wrong password']
+      };
+      expect(gen.next(res).value).toMatchObject(call(sleep, API_RQUEST_TS));
+      expect(gen.next().value).toMatchObject(put(SigninError(res.errors)));
+      expect(gen.next().done).toBeTruthy();
+    });
+
+    test('Server error', () => {
+      const data = {
+        login: 'login',
+        password: 'password'
+      };
+      const gen = signin({
+        type: SigninTypes.SIGNIN_REQUEST,
+        payload: data
+      });
+      try {
+        gen.throw('');
+      } catch (e) {
+        expect(gen.next().value).toMatchObject(put(SigninError(['signin.error.server'])));
+      }
+    });
+
+    test('Success', () => {
+      const data = {
+        login: 'login',
+        password: 'password'
+      };
+      const gen = signin({
+        type: SigninTypes.SIGNIN_REQUEST,
+        payload: data
+      });
+      expect(gen.next().value).toMatchObject(call(signValidate, data));
+      const errors = signValidate(data);
+      expect(gen.next(errors).value).toMatchObject(call(signinRequest, data));
+      const res = {
+        data: {
+          id: 1,
+          name: 'name',
+          email: '123@mail.ru',
+          age: 18
+        }
+      };
+      expect(gen.next(res).value).toMatchObject(call(sleep, API_RQUEST_TS));
+      expect(gen.next().value).toMatchObject(put(SigninSuccess(res.data)));
+      expect(gen.next().done).toBeTruthy();
     });
   });
 });
--- src/Saga/signin.ts
+++ src/Saga/signin.ts
@@ -5,12 +5,14 @@ import signValidate from '../Validate/signin';
 import {l8n} from 'src/l8n';
 import sleep from '../Utils/sleep';
 
+export const API_RQUEST_TS = 1000;
+
 export function* signin(action: ISigninRequest) {
   const errors = yield call(signValidate, action.payload);
   if (!errors || errors.length === 0) {
     try {
       const res = yield call(signinRequest, action.payload);
-      yield call(sleep, 1000);
+      yield call(sleep, API_RQUEST_TS);
       if (res.errors) {
         yield put(SigninError(res.errors));
       } else {
